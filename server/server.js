47f90fd00d3a31497df64a25723836edf144eb81
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-16 11:40:11 -0600

    ready for deployment

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/",function(req,res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+})
+
+
+/// heroku will supply its on env number
+const port = process.env.PORT || 4005;
+
+app.listen(port, ()=>{
+    console.log(`wee vibin on port ${port}`)
+});
