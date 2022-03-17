3ab84c347b6bf86dd63d19a99a4da2a84e537bd6
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-17 13:49:51 -0600

    devops lab roll bar ignition

--- server/server.js
+++ server/server.js
@@ -2,21 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '4070a11f0c9f40329ebb49da4c5ce872',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/",function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+//Middleware(below you only need to serve it up once)
+app.use(express.static(path.join(__dirname, "../public")))
 
-app.get("/styles", function(req,res){
-    res.sendFile(path.join(__dirname,"../public/index.css"))
-})
+// research how to serve another static folder
 
-/// heroku will supply its on env number
-const port = process.env.PORT || 4005;
+//endpoints
+// app.get("/",function(req,res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
+
+// app.get("/styles", function(req,res){
+//     res.sendFile(path.join(__dirname,"../public/index.css"))
+// })
+
+// /// heroku will supply its on env number
+// const port = process.env.PORT || 4005;
 
 app.listen(port, ()=>{
     console.log(`wee vibin on port ${port}`)
